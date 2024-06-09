367f1eaa6bdab8eb06a4c1ea83de9887dc26b2ef
Aditya Shimpi <115714741+the-knight-rider@users.noreply.github.com>
2024-06-09 22:27:31 +0530

    .

--- BagGlam/src/app/app.component.ts
+++ BagGlam/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
