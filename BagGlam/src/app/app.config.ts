237afd65568339112d3c7a0a581943c43f622824
Aditya Shimpi <115714741+the-knight-rider@users.noreply.github.com>
2024-06-09 13:27:14 +0530

    .

--- BagGlam/src/app/app.config.ts
+++ BagGlam/src/app/app.config.ts
@@ -3,8 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { ShopComponent } from './shop/shop.component';
-import { HomeComponent } from './home/home.component';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()]
